3c10a60d89d88dcd384f0e9ba3affe06132b839c
Teodora Stereciu <teostereciu@gmail.com>
2022-06-13 12:43:19 +0200

    room5

--- src/main/java/nl/ai/rug/oop/rpg/model/NPC.java
+++ src/main/java/nl/ai/rug/oop/rpg/model/NPC.java
@@ -56,6 +56,8 @@ public class NPC {
         NPCNumber = 3;
         NPCname = "melvin";
         roomNumber = 5;
+        coords.put("x", 100);
+        coords.put("y", 100);
     }
     private void Alex() {
         this.NPCNumber = 4;
